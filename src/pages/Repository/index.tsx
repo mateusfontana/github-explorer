47f2d9bcd6512c87a5c91ae68f18b4089e315f34
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2020-08-19 07:26:29 -0300

    feat: navigate to repository page when clicking on repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
+  const { params } = useRouteMatch();
   return <h1>Repository</h1>;
 };
 
